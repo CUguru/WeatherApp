b03e10ea4e9d539cf51b85c7b8c12ad7bc50d57c
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-01-05 14:58:23 -0500

    added the center property

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,10 +1,19 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples!!!!</h3>
-      <p>New Examples paragraph</p>
+      <h1 className="text-center">Examples!!!!</h1>
+      <p>Here are a few example locations to try out.</p>
+      <ol>
+        <li>
+          <Link to='/?location=Lagos'>Lagos, Nigeria</Link>
+        </li>
+        <li>
+          <Link to='/?location=Maui'>Maui, Hawaii</Link>
+        </li>
+      </ol>
     </div>
   )
 };
