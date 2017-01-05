52223c2ca04e22ebf563709b06685e1da05692df
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-01-04 22:32:38 -0500

    Rename the example page

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,10 +3,9 @@ var React = require('react');
 var Examples = (props) => {
   return(
     <div>
-      <h3>Examples Component!!</h3>
+      <h3>Examples!!</h3>
       <p>New Examples paragraph</p>
     </div>
-
   )
 };
 module.exports = Examples;
