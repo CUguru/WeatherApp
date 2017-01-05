00686aa50de6f78fdf04787f63c3f1f1741e665f
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-01-05 14:40:48 -0500

    added images folder

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,30 +1,27 @@
 var React = require('react');
 var {Link, IndexLink} = require('react-router');
 
-// var Nav = React.createClass({
-//   render: function() {
-//     return(
-//       <div>
-//         <h2>Nav component</h2>
-//         <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-//         <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-//         <Link to='/examples' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-//
-//       </div>
-//     );
-//   }
-// });
-
-var Nav = () => {
-  return(
-    <div>
-      <h2>Nav component</h2>
-      <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
-      <Link to='/examples' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
-
-    </div>
-  );
-};
-
+var Nav = React.createClass({
+  render: function() {
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="weather-fetch">WeatherFetch</li>
+            <li>
+              <IndexLink to='/' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
+            </li>
+            <li>
+              <Link to='/about' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>About</Link>
+            </li>
+            <li>
+              <Link to='/examples' activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right"></div>
+      </div>
+    );
+  }
+});
 module.exports = Nav;
