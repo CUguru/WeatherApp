0a22b90a4021aadcacf10e5155c0948d7b3662a2
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-01-04 22:16:31 -0500

    Prep repo for heroku

--- server.js
+++ server.js
@@ -2,8 +2,17 @@ var express = require('express'); // have acces to the whole express API
 
 // Creat our own app
 var app = express();
-app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, function() {
-  console.log("express server is up on port 3000");
+app.use(function(req, res, next) {
+  if(req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
+
+app.use(express.static('public'));
+app.listen(PORT, function() {
+  console.log("express server is up on port " + PORT);
 });
