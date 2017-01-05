a96586960bfbaa65415e3eadb37f6d44742a370a
Chibuzo Uguru <uguru@sheridanc.on.ca>
2017-01-04 19:52:15 -0500

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -2,7 +2,11 @@ var React = require('react');
 
 var Examples = (props) => {
   return(
-    <h3>Examples Component!!</h3>
+    <div>
+      <h3>Examples Component!!</h3>
+      <p>New Examples paragraph</p>
+    </div>
+
   )
 };
 module.exports = Examples;
